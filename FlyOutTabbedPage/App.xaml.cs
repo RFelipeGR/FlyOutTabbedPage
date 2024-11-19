77d40254ecfa6f68fede7330967d5510abc63990
laboratorio <laboratorio@UPOAULA10316>
2024-11-19 08:37:09 -0500

    Agregar archivos de proyecto.

---
+++ FlyOutTabbedPage/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlyOutTabbedPage
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
